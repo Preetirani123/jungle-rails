ffd3b9c4ba5c85c9b378a703b6c7c0b976121192
Preetirani123 <rani.preeti3333@gmail.com>
2021-04-01 02:23:15 +0000

    add the instance variables in the controllers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   # http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASS"]
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
   def show
+    @products = Product.all.count
+    @categorys = Category.all.count
+    
   end
 end
