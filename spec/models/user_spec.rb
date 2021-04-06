621ad377fab85670a4d44cbdfe0cf1813e33a697
Preetirani123 <rani.preeti3333@gmail.com>
2021-04-06 00:13:27 +0000

    test first validation with valid data

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
+require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    # validation examples here
+    it "is valid with valid data" do
+      user = User.new(first_name: "Amit", last_name: "Kumar", email: "kumar@gmail.com", password: "Amit@123", password_confirmation: "Amit@123")
+      expect(user).to be_valid
+      expect(user.errors.full_messages).to be_empty
+    end
   end
 
   describe '.authenticate_with_credentials' do
