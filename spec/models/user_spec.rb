2f93fdfdabe08d440623d745a107ea5091fcca84
Preetirani123 <rani.preeti3333@gmail.com>
2021-04-06 01:05:57 +0000

    recreate the user_spec file with first test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,14 @@
 require 'rails_helper'
+
 RSpec.describe User, type: :model do
-
   describe 'Validations' do
-    it "with valid data" do
-      user = User.new(first_name: "Amit", last_name: "Kumar", email: "kumar@gmail.com", password: "Amit@123", password_confirmation: "Amit@123")
-      expect(user).to be_valid
-      expect(user.errors.full_messages).to be_empty
-    end
-
-
-    it "Not valid if password or pass_confirmation not match " do
-      user = User.new(first_name: "Amit", last_name: "Kumar", email: "kumar@gmail.com", password: "Amit@123", password_confirmation: "Preeti@123")
-      expect(user).not_to be_valid
-      expect(user.errors.full_messages).to include ("Password confirmation not match Password")
-      
+    it 'should create a user with all fields' do
+      user = User.create(first_name: 'Ana',last_name: 'Kumar', email: 'ana@gmail.com',password: 'ana@123',password_confirmation: 'ana@123')
+      expect(user).to (be_valid)
     end
   end
 
   describe '.authenticate_with_credentials' do
     # examples for this class method here
   end
-
 end
