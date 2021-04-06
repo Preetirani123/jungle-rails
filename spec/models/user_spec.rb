93aeaef79ba50f82eac46f59dee4acd5b50d8e49
Preetirani123 <rani.preeti3333@gmail.com>
2021-04-06 00:19:44 +0000

    password and password_confirmation not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    it "is valid with valid data" do
+    it "with valid data" do
       user = User.new(first_name: "Amit", last_name: "Kumar", email: "kumar@gmail.com", password: "Amit@123", password_confirmation: "Amit@123")
       expect(user).to be_valid
       expect(user.errors.full_messages).to be_empty
+    end
+
+
+    it "Not valid if password or pass_confirmation not match " do
+      user = User.new(first_name: "Amit", last_name: "Kumar", email: "kumar@gmail.com", password: "Amit@123", password_confirmation: "Preeti@123")
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to include ("Password confirmation not match Password")
+      
     end
   end
 
